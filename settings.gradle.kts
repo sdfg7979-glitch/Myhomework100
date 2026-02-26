c08b9c04a3220ba280da19cc2728b726e3ed0d41
sdfg79 <sdfg8080@gmail.com>
2026-02-26 17:19:33 +0900

    todo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
 
 rootProject.name = "My Application"
 include(":app")
-include(":old_ch9")
-include(":old_ch10")
-include(":ch6")
-include(":ch7")
+include(":app:ch8")
+include(":ch9")
+include(":ch10")
+include(":ch11")
