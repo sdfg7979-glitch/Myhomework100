24f0586288cec3b14567173915b7a50f6badaac7
Patrick <patrick@ibebu.net>
2026-03-16 17:56:18 +0900

    쓰잘데기 없는 주석 입니다.

--- ch10/src/main/java/com/example/ch10/MainActivity.kt
+++ ch10/src/main/java/com/example/ch10/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
+//쓰잘데기 없는 주석입니다.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
